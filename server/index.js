dcd77f214f35dc5a4c6357d85561a41bfe35f6af
shubham hiwale <shubhamvhiwale@gmailc.om>
2023-01-06 11:31:35 +0530

    update 2

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.listen(8080, (req, res) => {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  console.log("server running...");
+});
